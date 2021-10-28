cb2173bcafbab491e06e93519116da28a8417c14
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-10-28 15:23:20 +0300

    Custom Apple keymap for YMD75

--- keyboards/ymd75/config.h
+++ keyboards/ymd75/config.h
@@ -21,8 +21,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-// Changed the USB Vendor ID and Product ID to pretend this is an Apple keyboard
+// Changed the USB Vendor ID (VID) and Product ID (PID) to pretend this is an Apple keyboard
 // See: https://github.com/qmk/qmk_firmware/issues/2179#issuecomment-569851038
+// I used the PID of the Apple Aluminium Keyboard (ANSI) to trick macOS into giving me the same shortcuts
+// See: https://elixir.bootlin.com/linux/latest/source/drivers/hid/hid-ids.h#L93
 //#define VENDOR_ID       0x20A0
 //#define PRODUCT_ID      0x422D
 #define VENDOR_ID       0x05AC
@@ -31,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MANUFACTURER    YMDK
 // Changed the USB Product string to a custom version
 //#define PRODUCT         YMD75 / MT84
-#define PRODUCT         YMD75 Apple Mod
+#define PRODUCT         YMD75 (Apple Mod)
 
 #define BACKLIGHT_LEVELS 12
 
@@ -44,3 +46,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 15
 #define RGBLIGHT_VAL_STEP 18
 #define DYNAMIC_KEYMAP_LAYER_COUNT 3
+
+// Enable Apple fn key support
+#define APPLE_FN_ENABLE
