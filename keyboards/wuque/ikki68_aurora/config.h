9180a5a351513bb8cd1c8e117541bc6f9646190c
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-04-03 20:54:06 +0300

    Ikki68 Aurora: fix Apple fn support

--- keyboards/wuque/ikki68_aurora/config.h
+++ keyboards/wuque/ikki68_aurora/config.h
@@ -20,8 +20,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x706B
-#define PRODUCT_ID      0x0011
+// Changed the USB Vendor ID (VID) and Product ID (PID) to pretend this is an Apple keyboard
+// See: https://github.com/qmk/qmk_firmware/issues/2179#issuecomment-569851038
+// I used the PID of the Apple Aluminium Keyboard (ANSI) to trick macOS into giving me the same shortcuts
+// See: https://elixir.bootlin.com/linux/latest/source/drivers/hid/hid-ids.h#L93
+// #define VENDOR_ID       0x706B
+// #define PRODUCT_ID      0x0011
+#define VENDOR_ID       0x05AC
+#define PRODUCT_ID      0x024F
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    wuque studio
 #define PRODUCT         ikki68 Aurora
@@ -70,3 +76,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //     #define RGBLIGHT_EFFECT_RGB_TEST
 //     #define RGBLIGHT_EFFECT_ALTERNATING
 #endif
+
+// Enable Apple fn key support
+#define APPLE_FN_ENABLE
